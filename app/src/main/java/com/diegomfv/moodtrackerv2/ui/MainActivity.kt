40d9b02f301f9d859d316a4f573b56eddf6d6969
diegomfv <diego.fajardo.villena@gmail.com>
2020-01-28 22:12:59 +0000

    gradle update

--- app/src/main/java/com/diegomfv/moodtrackerv2/ui/MainActivity.kt
+++ app/src/main/java/com/diegomfv/moodtrackerv2/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.diegomfv.moodtrackerv2
+package com.diegomfv.moodtrackerv2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.diegomfv.moodtrackerv2.R
 
 class MainActivity : AppCompatActivity() {
 
