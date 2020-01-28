5ee55630dc363ebb0ccc57e07568083ecc6a8a47
diegomfv <diego.fajardo.villena@gmail.com>
2020-01-28 22:29:03 +0000

    Add MainActivityViewModel

--- app/src/main/java/com/diegomfv/moodtrackerv2/ui/main/MainActivity.kt
+++ app/src/main/java/com/diegomfv/moodtrackerv2/ui/main/MainActivity.kt
@@ -3,8 +3,12 @@ package com.diegomfv.moodtrackerv2.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.diegomfv.moodtrackerv2.R
+import org.koin.android.scope.currentScope
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainActivityViewModel: MainActivityViewModel by currentScope.viewModel(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
