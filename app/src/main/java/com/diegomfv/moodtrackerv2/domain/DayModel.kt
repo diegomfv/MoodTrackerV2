607518d832cf301b59b14cff9cf33f53380666be
diegomfv <diego.fajardo.villena@gmail.com>
2021-02-10 19:26:17 +0000

    Update Domain model

--- app/src/main/java/com/diegomfv/moodtrackerv2/domain/DayModel.kt
+++ app/src/main/java/com/diegomfv/moodtrackerv2/domain/DayModel.kt
@@ -1,17 +1,11 @@
 package com.diegomfv.moodtrackerv2.domain
 
-import com.diegomfv.moodtrackerv2.constants.MOOD_NORMAL
-import com.diegomfv.moodtrackerv2.constants.listOfDaysAsString
-
+/**
+ * UI Model
+ */
 data class DayModel (
-    val day: Int,
-    val dayAsString: String,
+    val itemId: String,
+    val day: String,
     val mood: Int,
-    val comment: String? = null
-) {
-
-    companion object {
-        fun emptyDay (day: Int) = DayModel(day, listOfDaysAsString[day], MOOD_NORMAL, "")
-    }
-
-}
+    val comment: String
+)
